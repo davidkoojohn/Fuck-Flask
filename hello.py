e754e1733007e16068d541017a31a13b00963c3f
davidkoojohn <davidkoojohn@gmail.com>
2018-08-11 17:05:41 +0800

    try flask route agin

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Index Page'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        print('hhh')
+    else:
+        print('j')
+
 
 
 @app.route('/user')
@@ -23,6 +32,11 @@ def user_show(username):
 @app.route('/post/<int:post_id>')
 def post_show(post_id):
     return 'post id is %d' % post_id
+
+
+@app.route('/post/<int:post_id>/<int:year>')
+def post_year_show(post_id, year):
+    return 'post id is %d' % (post_id + year)
 
 
 if __name__ == '__main__':

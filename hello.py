53000eea5b2e62e48e933825ac0f6c4ca407f267
davidkoojohn <davidkoojohn@gmail.com>
2018-08-11 16:40:13 +0800

    try flask router

--- hello.py
+++ hello.py
@@ -4,12 +4,31 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'hello flask!'
-    pass
+def index():
+    return 'Index Page'
+
+
+@app.route('/user')
+def user():
+    return 'user index'
+
+
+# 变量标记
+@app.route('/user/<username>')
+def user_show(username):
+    return 'username is %s' % username
+
+
+# 变量转换为特定类型 int, float, path
+@app.route('/post/<int:post_id>')
+def post_show(post_id):
+    return 'post id is %d' % post_id
+
 
 if __name__ == '__main__':
-    app.run()
+    # 开启调试模式的两种方法，可以修改代码重启服务
+    # app.debug = True
+    app.run(host='0.0.0.0', debug=True)
 
 
 
